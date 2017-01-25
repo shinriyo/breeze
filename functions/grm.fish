6e795de94f46230be90050864ed4a235da14aba8
shinriyo <shinriyo@shinriyos-MacBook.local>
2017-01-26 01:13:37 +0900

    $argv

--- functions/grm.fish
+++ functions/grm.fish
@@ -1,3 +1,3 @@
 function grm
-    git rm   
+    git rm $argv
 end
--- functions/mkrb.fish
+++ functions/mkrb.fish
@@ -1,3 +1,6 @@
 function mkrb
     # do something
+    string trim '#!/usr/bin/ruby
+' > ./$argv.rb
+    echo './'$argv'.rb file was created.'
 end
