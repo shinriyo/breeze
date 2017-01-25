32242b3900c74bb0e25f96936e424228adbffe06
shinriyo <shinriyo@shinriyos-MacBook.local>
2017-01-26 02:08:57 +0900

    $argv

--- functions/gc.fish
+++ functions/gc.fish
@@ -1,3 +1,43 @@
-function gc
-    git checkout $argv
+function __ga
+    # number
+    set res (string split "-" -- (string trim $argv))
+    set first $res[1]
+    set length (count $res)
+    set last ""
+
+    # >
+    if [ $length -gt 1 ]
+        set last $res[2]
+    else
+        # just one
+        set myarg $arr[$res]
+        git add $myarg
+        return
+    end
+
+    # first < last
+    if [ $last != '' ]
+        if [ $first -lt $last ]
+          #for i in (seq $first 1 $last)
+          for i in $res
+              set myarg $arr[$i]
+              git add $myarg
+          end
+        else
+          echo 'argument is not valid.'
+        end
+    else
+        set myarg $arr[$first]
+        git add $myarg
+    end
+    #echo $res[1]end
+end
+
+function ga
+    # TODO: space like, `ga 1 2 3`
+    set res (string split " " -- (string trim $argv))
+    for i in $res
+        #echo $i
+        __ga $i
+    end
 end
