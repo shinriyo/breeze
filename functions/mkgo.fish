4c49e078f463579f7c99f34c4e809fe12a224d52
杉田臣輔 <shinsuke.sugita@drecom.co.jp>
2017-01-26 12:33:42 +0900

    $argv

--- functions/mkgo.fish
+++ functions/mkgo.fish
@@ -1,3 +1,13 @@
 function mkgo
-    # do something
+    string trim '// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
+' > ./$argv.go
+    echo './'$argv'.go file was created.'
 end
