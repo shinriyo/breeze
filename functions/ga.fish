db64f5fc94889769dae96df93313870e91ec1d86
杉田臣輔 <shinsuke.sugita@drecom.co.jp>
2017-01-26 18:50:50 +0900

    $argv

--- functions/ga.fish
+++ functions/ga.fish
@@ -1,3 +1,15 @@
+function __git_add -a var
+    # is numeric 
+    if [ "$var" -eq "$var" ] 2>/dev/null
+        # number
+        set myarg $arr[$var]
+        git add $myarg
+    else
+        # not a number
+        git add $var
+    end
+end
+
 function __ga
     # number
     set res (string split "-" -- (string trim $argv))
@@ -10,8 +22,9 @@ function __ga
         set last $res[2]
     else
         # just one
-        set myarg $arr[$res]
-        git add $myarg
+        #set myarg $arr[$res]
+        #git add $myarg
+        __git_add $myarg
         return
     end
 
@@ -20,15 +33,17 @@ function __ga
         if [ $first -lt $last ]
           #for i in (seq $first 1 $last)
           for i in $res
-              set myarg $arr[$i]
-              git add $myarg
+              #set myarg $arr[$i]
+              #git add $myarg
+              __git_add $i
           end
         else
           echo 'argument is not valid.'
         end
     else
-        set myarg $arr[$first]
-        git add $myarg
+        #set myarg $arr[$first]
+        #git add $myarg
+        __git_add $first
     end
     #echo $res[1]end
 end
