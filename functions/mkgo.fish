f0dc73c858f7dc1d1d09b4299fd8af88a99516d9
杉田臣輔 <shinsuke.sugita@drecom.co.jp>
2017-01-30 13:05:23 +0900

    rb go

--- functions/mkgo.fish
+++ functions/mkgo.fish
@@ -1,10 +1,23 @@
 function mkgo
+    # check argument
     if count $argv > /dev/null
     else
-       echo 'Please input file name for argument.'
-       return
+        echo 'Please input file name for argument.'
+        return
     end
 
+    # check argument
+    if test -e './'$argv'.go'
+        echo -sn './'$argv'.go already exists. Continue to override? [Y/n] ' > /dev/stderr
+        __fisher_get_key | read -l yn
+        echo $yn
+        switch "$yn"
+            case n N
+            return 1
+        end
+    end
+
+    # make go file
     string trim '// You can edit this code!
 // Click here and start typing.
 package main
