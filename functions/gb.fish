3af0b1ce7d241edd2858998ca6656e1519027495
shinriyo <shinriyo@shinriyos-MacBook.local>
2017-02-11 21:16:31 +0900

    indent for gb

--- functions/gb.fish
+++ functions/gb.fish
@@ -14,50 +14,49 @@ function gb
         end
     end
 
-  set check_count (git branch)
-  set length (count $check_count)
-    if [ $length -gt 1 ]
-    # reset
-    set arr ""
-  end
-
-  # increment
-  set i 0
-
-  for item in (git branch)
-
-    #increment
-    set i (math $i + 1)
-
-    # check * contain
-    set res (string split " " -- (string trim $item))
-    set length (count $res)
-    # >
-    set is_contain true
-    set name ""
-    # more than one
-    if [ $length -gt 1 ]
-        # with *
-        set name (string trim $res[2])
-    else #only one
-        set name (string trim $item)
+    set check_count (git branch)
+    set length (count $check_count)
+        if [ $length -gt 1 ]
+        # reset
+        set arr ""
     end
 
-    set arr[$i] $name
+    # increment
+    set i 0
 
-    # *
-    if [ $length -gt 1 ]
+    for item in (git branch)
+        #increment
+        set i (math $i + 1)
+
+        # check * contain
+        set res (string split " " -- (string trim $item))
+        set length (count $res)
+        # >
+        set is_contain true
+        set name ""
+        # more than one
+        if [ $length -gt 1 ]
+            # with *
+            set name (string trim $res[2])
+        else #only one
+            set name (string trim $item)
+        end
+
+        set arr[$i] $name
+
+        # *
+        if [ $length -gt 1 ]
+            # text without new line
+            echo -ne '* '
+        else
+            # just blank
+            echo -ne '  '
+        end
+
         # text without new line
-        echo -ne '* '
-    else
-        # just blank
-        echo -ne '  '
+        echo -ne [$i]' '
+        set_color green
+        echo $name
+        set_color normal
     end
-
-    # text without new line
-    echo -ne [$i]' '
-    set_color green
-    echo $name
-    set_color normal
-  end
 end
